a468252a8d33e6d3cf3972279924847cdc4afeb8
Sagar-Ahir <sagar.ahir@equisinfo.in>
2024-10-11 17:48:56 +0530

    initial commit

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,27 +1,23 @@
 import { Link, Stack } from 'expo-router';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
+
+import { Container } from '~/components/Container';
 
 export default function NotFoundScreen() {
   return (
     <>
       <Stack.Screen options={{ title: 'Oops!' }} />
-      <View style={styles.container}>
+      <Container>
         <Text style={styles.title}>This screen doesn't exist.</Text>
         <Link href="/" style={styles.link}>
           <Text style={styles.linkText}>Go to home screen!</Text>
         </Link>
-      </View>
+      </Container>
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,5 @@
-import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, View } from "react-native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Stack } from 'expo-router';
 
-import Colors from "@/src/Utils/Colors";
-
-export default function RootLayout() {
-  return (
-    <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1, backgroundColor: Colors.background }}>
-        <View style={{ flex: 1, backgroundColor: Colors.background }}>
-          <StatusBar style="light" />
-          <Stack>
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen
-              name="deviceDetails"
-              options={{ headerShown: false }}
-            />
-          </Stack>
-        </View>
-      </SafeAreaView>
-    </SafeAreaProvider>
-  );
+export default function Layout() {
+  return <Stack />;
 }
